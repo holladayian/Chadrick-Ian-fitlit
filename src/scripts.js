d6959bd39dc7d945528123e24c6b687a95e67363
Ian Holladay <holladayian@gmail.com>
2020-08-27 18:36:51 -0600

    create findBeginningOfWeek function

--- src/scripts.js
+++ src/scripts.js
@@ -10,3 +10,13 @@ function selectDate(day) {
   console.log(theSelectedDate.format('YYYY-MM-DD'));
   return theSelectedDate.format('YYYY-MM-DD');
 }
+
+function findBeginningOfWeek(day) {
+  const selectedDay = moment(selectDate(day));
+  const weekStartDate = selectedDay.subtract(7, 'days');
+  console.log(weekStartDate.format('YYYY-MM-DD'));
+  return theSelectedDate.format('YYYY-MM-DD');
+}
+
+selectDate("2019/06/15");
+findBeginningOfWeek("2019/06/15");
