ac2683c5b2bc7302e253a107142ddb22434b56ba
Ian Holladay <holladayian@gmail.com>
2020-08-27 18:36:13 -0600

    create selectDate function

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// var moment = require('moment');
+let theSelectedDate;
+
+
+window.addEventListener('click', findBeginningOfWeek);
+
+
+function selectDate(day) {
+  theSelectedDate = moment(day, 'YYYY-MM-DD');
+  console.log(theSelectedDate.format('YYYY-MM-DD'));
+  return theSelectedDate.format('YYYY-MM-DD');
+}
