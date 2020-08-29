98377b814900590c9d2b89ead85520f79afd07ca
Ian Holladay <holladayian@gmail.com>
2020-08-29 16:42:46 -0600

    It should be a function

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -1,1 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+const userSamples = require('../data/userSamples');
+const hydrationSamples = require('../data/hydrationSamples');
+const HydrationRepository = require('../src/HydrationRepository');
+const UserHydration = require('../src/userHydration');
 
+describe('HydrationRepository', () => {
+  let hydroRepo;
+  beforeEach( () => {
+    hydroRepo = new HydrationRepository(hydrationSamples);
+  });
+
+  it('should be a function', () => {
+    expect(HydrationRepository).to.be.a('function');
+  });
+
+  it('should be an instance of the HydrationRepository Class', () => {
+    expect(hydroRepo).to.be.an.instanceof(HydrationRepository);
+  });
+});
