02fa2c6e264339f6f2bc5297438bdce7860b6885
Ian Holladay <holladayian@gmail.com>
2020-08-30 16:17:56 -0600

    add ActivityRepository test to test for instances of UserActivity

--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -5,13 +5,19 @@ const ActivityRepository = require('../src/ActivityRepository');
 
 
 describe('ActivityRepository', () => {
-  let activityRepo;
+  let activityRepo, userActivity;
   beforeEach( () => {
     activityRepo = new ActivityRepository();
+    userActivity = new UserActivity();
+
   });
 
   it('should be a function', () => {
     expect(ActivityRepository).to.be.a('function');
   });
+
+  it('shoiuld instantiate a new UserActivity', =>{
+    expect(activityRepo.instantiateActiveUser()).to.be.an.instanceof(UserActivity)
+  })
 
 });
