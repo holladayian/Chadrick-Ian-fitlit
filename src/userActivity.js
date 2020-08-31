42139f52e078fb953c584a0f04e80956b83d3d29
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-08-30 19:02:11 -0600

    Create and test findMilesWalkedSpecificDay method, create findStartDateMethod to access user stride length needed

--- src/userActivity.js
+++ src/userActivity.js
@@ -1,13 +1,25 @@
 const moment = require('moment');
+const UserRepository = require('../src/UserRepository');
 
 class UserActivity {
-  constructor() {
-
+  constructor(user) {
+    this.userActivityInformation = user;
   }
   // For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
   // Should this be a Time class feature?
-
-  
+  // I need to calculate miles walked using number of steps, where each step equals the strid length from the userSamples
+  findMilesWalkedSpecificDay(date) {
+    let userSteps = this.findStartDateInfo(date).numSteps;
+    let userStrideLength = this.findUserStride();
+    let userMilesWalked = (userSteps * userStrideLength) / 5280;
+    return Math.round(userMilesWalked * 100) / 100;
+  }
+  findStartDateInfo(date) {
+    return this.userActivityInformation.find(datum => datum['date'] === date);
+  }
+  findUserStride() {
+    return new UserRepository().findUserInfo(this.userActivityInformation[0].userID).strideLength;
+  }
 }
 
 module.exports = UserActivity;
--- test/UserActivity-test.js
+++ test/UserActivity-test.js
@@ -1,15 +1,25 @@
 const chai = require('chai');
 const expect = chai.expect;
 const UserActivity = require('../src/UserActivity');
+const ActivityRepository = require('../src/ActivityRepository');
 
 describe('UserActivity', () => {
-  let userActivity;
+  let userActivity, activityRepo;
   beforeEach( () => {
-    userActivity = new UserActivity();
+    // userActivity = new UserActivity();
+    activityRepo = new ActivityRepository();
+    userActivity = activityRepo.instantiateUserActivity(1);
   });
 
   it('should be a function', () => {
     expect(UserActivity).to.be.a('function');
   });
 
+  it('should return user stride length', () => {
+    expect(userActivity.findUserStride()).to.equal(4.3);
+  })
+
+  it('should return miles walked for a specific day', () => {
+    expect(userActivity.findMilesWalkedSpecificDay("2019/06/15")).to.equal(2.91);
+  })
 });
